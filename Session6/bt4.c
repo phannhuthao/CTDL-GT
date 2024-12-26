c0d59c0a7272b9d34be8eb72969ef5e01b0445c7
Phan Nhựt Hào (Ohayo) <149989588+phannhuthao@users.noreply.github.com>
2024-12-26 21:49:17 +0700

    bt4

---
+++ Session6/bt4.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
+
+int isEmpty() {
+    return top == -1;
+}
+
+int isFull() {
+    return top == MAX -1;
+}
+
+void push(int value) {
+    if (top == MAX - 1) {
+        printf("Ngan xep day.\n");
+        return;
+    }
+    stack[++top] = value;
+}
+
+void printStack() {
+    if (isEmpty()) {
+        printf("Ngan xep rong.\n");
+        return;
+    } else {
+        printf("Ngan khong rong.\n");
+    }
+
+    printf("Cac phan tu trong ngan xep tu tren xuong duoi la: \n");
+    for (int i = top; i >= 0; i--) {
+        printf("%d\n", stack[i]);
+    }
+}
+
+void pop() {
+    if (isEmpty()) {
+        printf("Ngan xep rong.\n");
+        return;
+    }
+
+    int value = stack[top];
+    top--;
+    printf("Phan tu da xoa: %d\n", value);
+}
+
+void peek() {
+    if (isEmpty()) {
+        printf("Ngan xep rong.\n");
+        return;
+    }
+    printf("Phan tu tren cung: %d\n", stack[top]);
+}
+
+int main() {
+    int n,  value;
+
+    printf("Nhap so luong phan tu trong ngan xep: ");
+    scanf("%d", &n);
+
+    if(n > 100) {
+        printf("Gia tri n phai be hon 100");
+        return 0;
+    }
+
+    for(int i = 0; i < n ; i++) {
+        printf("Phan tu thu: %d ", i + 1);
+        scanf("%d", &value);
+        push(value);
+    }
+
+    printStack();
+
+    pop();
+    printStack();
+
+    peek();
+
+    return 0;
+}
