a98c126b017c5872d029cdf06e3144796dc04066
Phan Nhựt Hào (Ohayo) <149989588+phannhuthao@users.noreply.github.com>
2025-01-07 21:38:44 +0700

    s13

---
+++ Session13/bt1.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n, int ascending) {
+    int swapped;
+    for (int i = 0; i < n-1; i++) {
+        swapped = 0;
+        for (int j = 0; j < n-i-1; j++) {
+            if ((ascending && arr[j] > arr[j+1]) || (!ascending && arr[j] < arr[j+1])) {
+                // Hoán đổi hai phần tử
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                swapped = 1;
+            }
+        }
+        if (!swapped) {
+            break;
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    printf("Nhap so luong mang: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Nhap %d phan tu trong mang: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("before: ");
+    printArray(arr, n);
+
+    bubbleSort(arr, n, 1);
+
+    printf("Mang sau khi sap xep: ");
+    printArray(arr, n);
+
+    return 0;
+}
