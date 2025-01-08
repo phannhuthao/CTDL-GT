6c35c7ac32ca0ac51104f29783c3206b11f4e339
Phan Nhựt Hào (Ohayo) <149989588+phannhuthao@users.noreply.github.com>
2025-01-08 21:36:35 +0700

    ss14

--- Session14/bt2.c
+++ Session14/bt2.c
@@ -1,3 +1,39 @@
-//
-// Created by DELL on 1/8/2025.
-//
+#include <stdio.h>
+
+int binarySearchRecursive(int arr[], int left, int right, int target) {
+    if (left > right) {
+        return -1;
+    }
+
+    int mid = left + (right - left) / 2;
+
+    if (arr[mid] == target) {
+        return mid;
+    }
+
+    if (arr[mid] < target) {
+        return binarySearchRecursive(arr, mid + 1, right, target);
+    }
+    else {
+        return binarySearchRecursive(arr, left, mid - 1, target);
+    }
+}
+
+int main() {
+    int arr[] = {1, 3, 5, 7, 9, 11, 13, 15};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target;
+
+    printf("Nhap phan tu can tim: ");
+    scanf("%d", &target);
+
+    int result = binarySearchRecursive(arr, 0, n - 1, target);
+
+    if (result != -1) {
+        printf("Phan tu %d đuoc tim thay tai vi tri %d.\n", target, result);
+    } else {
+        printf("Khong tim thay phan tu %d trong mang.\n", target);
+    }
+
+    return 0;
+}
